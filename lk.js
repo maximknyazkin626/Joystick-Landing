cbfa40670efdc438cf6e15c9b12b4f3a4a450ec9
knyazkin maxim <knyazkinmaxim626@gmail.com>
2025-04-08 14:53:29 +0300

    1.21 LK Login and Registry

--- lk.js
+++ lk.js
@@ -1,40 +1,42 @@
 /////////Регистрация: Получение и запись данных//////
-const regLogin = document.getElementById('regLogin');
-const regPas = document.getElementById('regPas');
-const regButton = document.getElementById('registerButton');
-const regForm = document.getElementById('registerContent');
+const regLogin = document.getElementById("regLogin");
+const regPas = document.getElementById("regPas");
+const regButton = document.getElementById("registerButton");
+const regForm = document.getElementById("registerContent");
 
-regForm.addEventListener('submit', event => {
-    event.preventDefault(); ///предотвращение перезагрузки страницы
-    let login = regLogin.value;
-    alert(login);
-    let password = regPas.value;
-    alert(password);
+regForm.addEventListener("submit", (event) => {
+  event.preventDefault(); ///предотвращение перезагрузки страницы
+  let login = regLogin.value;
+  alert(login);
+  let password = regPas.value;
+  alert(password);
+
+  if (localStorage.getItem(login) !== null) {
+    alert("Этот логин уже занят, придумайте другой");
+  } else {
     localStorage.setItem(login, password);
-    console.log(localStorage);
-    login.value = '';
-    password.value = '';
-})
-
+  }
+  console.log(localStorage);
+  regLogin.value = "";
+  regPas.value = "";
+});
 
 ///////Аутентификация при входе в Личный кабинет/////
-const signForm = document.getElementById('signForm');
-const signLogin = document.getElementById('signLogin');
-const signPas = document.getElementById('signPas');
+const signForm = document.getElementById("signForm");
+const signLogin = document.getElementById("signLogin");
+const signPas = document.getElementById("signPas");
 
-signForm.addEventListener('submit', event => {
-    event.preventDefault();
+signForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let signLog = signLogin.value;
+  let signPassword = signPas.value;
 
-
-    let signLog = signLogin.value;
-    let signPassword = signPas.value;
-    let regPasword = localStorage.getItem('123');
-    let regLog = localStorage.key(0);
-    if (signLog === regLog && signPassword === regPasword) {
-        alert(`Вы успешно зашли ${signLog}!!!`);
-    } else {
-        alert('Неверный логин или пароль');
-    }
-    signLog.textContent = '';
-    signPassword.textContent = '';
-})
+  if (localStorage.getItem(signLog) === signPassword) {
+    alert(`Вы успешно зашли ${signLog}!!!`);
+  } else {
+    alert("Неверный логин или пароль");
+  }
+  //   signLog.textContent = "";
+  signLogin.value = "";
+  signPas.value = "";
+});
